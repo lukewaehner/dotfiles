88d9d9d53e45e2e465f2625569f9be148fa70eab
Luke Waehner <luke.waehner@gmail.com>
2026-05-01 12:22:24 -0400

    fix: restore lua_ls — was accidentally disabled in swift-lsp config

--- nvim/.config/nvim/lua/plugins/swift-lsp.lua
+++ nvim/.config/nvim/lua/plugins/swift-lsp.lua
@@ -3,7 +3,6 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
-	lua_ls = false,
         sourcekit = {
           cmd = { "sourcekit-lsp" },
           filetypes = { "swift" },
